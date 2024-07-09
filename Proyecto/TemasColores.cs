fc5116ce6aa64b6ad5236e822e71b0a709796dd8
makymanrs <132008304+makymanrs@users.noreply.github.com>
2024-07-08 21:59:24 -0600

    Nuevos Cambios

--- Proyecto/TemasColores.cs
+++ Proyecto/TemasColores.cs
@@ -13,31 +13,31 @@ namespace Proyecto
         public static Color SecondaryColor { get; set; }
         public static List<string> listaColores = new List<string>() {
             "#3F51B5", // Índigo
-            "#009688", // Verde Azulado
-            "#FF5722", // Naranja Profundo
-            "#607D8B", // Gris Azulado
-            "#FF9800", // Naranja
-            "#9C27B0", // Púrpura
-            "#2196F3", // Azul
-            "#EA676C", // Coral Claro
-            "#E41A4A", // Carmesí
-            "#5978BB", // Azul Yonder
-            "#018790", // Laguna Azul
-            "#0E3441", // Gris Pizarra Oscuro
-            "#00B0AD", // Verde Azulado Claro
-            "#721D47", // Frambuesa Oscura
-            "#EA4833", // Naranja Rojo
-            "#EF937E", // Salmón Claro
-            "#F37521", // Naranja Zanahoria
-            "#A12059", // Frambuesa Oscura
-            "#126881", // Cerúleo Profundo
+           // "#009688", // Verde Azulado
+           // "#FF5722", // Naranja Profundo // me gusta
+           // "#607D8B", // Gris Azulado
+           // "#FF9800", // Naranja
+            "#9C27B0", // Púrpura // me gusta
+           // "#2196F3", // Azul
+           //"#EA676C", // Coral Claro
+            "#E41A4A", // Carmesí // me gusta
+          //  "#5978BB", // Azul Yonder
+          // "#018790", // Laguna Azul
+           "#0E3441", // Gris Pizarra Oscuro // me gusta
+         // "#00B0AD", // Verde Azulado Claro
+          // "#721D47", // Frambuesa Oscura
+          //  "#EA4833", // Naranja Rojo
+           // "#EF937E", // Salmón Claro
+           // "#F37521", // Naranja Zanahoria
+           // "#A12059", // Frambuesa Oscura
+           // "#126881", // Cerúleo Profundo
           //  "#8BC240", // Verde Amarillo
-            "#364D5B", // Carbón
-            "#C7DC5B", // Verde Amarillo
-            "#0094BC", // Azul Pacífico
-            "#E4126B", // Magenta
-            "#43B76E", // Verde Mar Medio
-            "#7BCFE9", // Azul Cielo
+            "#364D5B", // Carbón // me gusta
+         //   "#C7DC5B", // Verde Amarillo
+         //   "#0094BC", // Azul Pacífico
+            //"#E4126B", // Magenta
+            "#43B76E", // Verde Mar Medio // megusta
+          //  "#7BCFE9", // Azul Cielo
             "#B71C46"  // Granate
         };
 
